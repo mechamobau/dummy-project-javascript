a1878a3b48ed3879534ad419b3d6fbe2962e2115
Lucas Viana <contato@lucasviana.dev>
2021-06-07 21:32:31 -0300

    :construction: creates App structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.25rem;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1rem;
+`;
+
+const Container = styled.main`
+  display: block;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 export default function App() {
-  return <h1>Welcome to the Basic Template for React + TypeScript!</h1>;
+  return (
+    <Container>
+      <Title>Welcome to the Basic Template for React + TypeScript!</Title>
+      <Subtitle>Subtítulo</Subtitle>
+    </Container>
+  );
 }
