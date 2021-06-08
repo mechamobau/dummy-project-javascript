74c79d83f2addb9f3b10f5cab6286d332d5ea91a
Lucas Viana <contato@lucasviana.dev>
2021-06-07 22:32:32 -0300

    :sparkles: creates COLORS constant

---
+++ src/constants/colors.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef Colors
+ * @property {string} primary
+ * @property {string} secondary
+ * @property {string} tertiary
+ * @property {string} background
+ */
+
+/**
+ * Constante que contém a paleta de cores usada no projeto.
+ * @type {Colors}
+ */
+const COLORS = {
+    primary: '#2d5d62',
+    secondary: '#5e8b73',
+    tertiary: '#a7c4bc',
+    background: '#dfeeea'
+}
+
+export default COLORS;
